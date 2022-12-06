40f86946688daa6076e80917fcfb1979e165cf65
SergeiVorobev <vorobevse86@gmail.com>
2022-12-06 15:13:45 +0100

    update main

--- suggestions/admin.py
+++ suggestions/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import CategoryQuestion, Question, Answer, Suggestion
 
 # Register your models here.
+admin.site.register(CategoryQuestion)
+admin.site.register(Question)
+admin.site.register(Answer)
+admin.site.register(Suggestion)
--- user/models.py
+++ user/models.py
@@ -1,3 +1,46 @@
+from django import forms
 from django.db import models
+from django.contrib.auth.models import User
+from PIL import Image
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    avatar = models.ImageField(default='default.jpg', upload_to='profile_images')
+    bio = models.TextField()
+
+    def __str__(self):
+        return self.user.username
+
+    # resizing images
+    def save(self, *args, **kwargs):
+        super().save()
+
+        img = Image.open(self.avatar.path)
+
+        if img.height > 100 or img.width > 100:
+            new_img = (100, 100)
+            img.thumbnail(new_img)
+            rgb_im = img.convert('RGB')
+            rgb_im.save(self.avatar.path)
+
+class UpdateUserForm(forms.ModelForm):
+    username = forms.CharField(max_length=100,
+                               required=True,
+                               widget=forms.TextInput(attrs={'class': 'form-control'}))
+    email = forms.EmailField(required=True,
+                             widget=forms.TextInput(attrs={'class': 'form-control'}))
+
+    class Meta:
+        model = User
+        fields = ['username', 'email']
+
+
+class UpdateProfileForm(forms.ModelForm):
+    avatar = forms.ImageField(widget=forms.FileInput(attrs={'class': 'form-control-file'}))
+    bio = forms.CharField(widget=forms.Textarea(attrs={'class': 'form-control', 'rows': 5}))
+
+    class Meta:
+        model = Profile
+        fields = ['avatar', 'bio']
