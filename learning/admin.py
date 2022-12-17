ce01a8a3bf2d79ad05c45fe2a1d0701a1fe1fd11
SergeiVorobev <vorobevse86@gmail.com>
2022-12-17 10:11:27 +0100

    feat: quiz functionality added

--- learning/admin.py
+++ learning/admin.py
@@ -1,6 +1,16 @@
 """Models registration for Admin"""
 from django.contrib import admin
-from .models import Material
+from .models import Material, Quiz, Answer, Question, Result
 
 # Register your models here.
+class AnswerInline(admin.TabularInline):
+    model = Answer
+
+class QuestionAdmin(admin.ModelAdmin):
+    inlines = [AnswerInline]
+
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Answer)
+admin.site.register(Quiz)
+admin.site.register(Result)
 admin.site.register(Material)
