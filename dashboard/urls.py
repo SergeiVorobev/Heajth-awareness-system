31909f89d811edf956954d6bd2ad2dd0fdbf02c0
SergeiVorobev <vorobevse86@gmail.com>
2022-12-27 22:47:10 +0100

    feat: delete not needed code

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -6,12 +6,9 @@ from . import views
 app_name = 'dashboard'
 
 urlpatterns = [
-    # path('', views.home, name="users-home"),
     path('show-health-data/<data_id>', views.show_health_data, name='show-health-data'),
     path('add-health-data/', views.add_health_data, name='add-health-data'),
     path('edit_health-data/<data_id>', views.edit_health_data, name="edit-health-data"),
     path('del_health-data/<data_id>', views.del_health_data, name="del-health-data"),
-    path('print_home_page/', views.print_home_page, name="print-home-page"),
-
 ]
 
--- dashboard/views.py
+++ dashboard/views.py
@@ -82,24 +82,3 @@ def del_health_data(request, data_id):
     event = HealthData.objects.get(pk=data_id)
     event.delete()
     return redirect('user:users-home')
-
-login_required(login_url='user:login')
-def print_home_page(request):
-    # Create a file-like buffer to receive PDF data.
-    buffer = io.BytesIO()
-
-    # Create the PDF object, using the buffer as its "file."
-    p = canvas.Canvas(buffer)
-
-    # Draw things on the PDF. Here's where the PDF generation happens.
-    # See the ReportLab documentation for the full list of functionality.
-    p.drawString(100, 100, "Hello world.")
-
-    # Close the PDF object cleanly, and we're done.
-    p.showPage()
-    p.save()
-
-    # FileResponse sets the Content-Disposition header so that browsers
-    # present the option to save the file.
-    buffer.seek(0)
-    return FileResponse(buffer, as_attachment=True, filename='hello.pdf')
